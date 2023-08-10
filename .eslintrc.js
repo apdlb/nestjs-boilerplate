eaaab9327d9e4173b9c3bd670e2b1aae074f402d
Alejandro Páez <alepdlb@gmail.com>
2023-08-10 13:25:57 +0200

    chore: remove prettier import order and add eslint plugin import

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,12 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
   root: true,
   env: {
@@ -17,6 +19,47 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    // turn on errors for missing imports
+    'import/no-unresolved': 'error',
+    /*
+     * "import/order" sorts the order of import declarations, ie:
+     *
+     * import b from "b";
+     * import { z, a } from "x";
+     */
+    'import/order': [
+      'warn',
+      {
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        groups: [
+          ['external', 'builtin'],
+          ['internal'],
+          ['index', 'sibling', 'parent'],
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+    /*
+     * Then, "sort-imports" complements "import/order" by sorting named imports,
+     * like so:
+     *
+     * import b from "b";
+     * import { a, z } from "x";
+     */
+    'sort-imports': [
+      'warn',
+      {
+        ignoreCase: true,
+        /*
+         * This option turns off declaration sorting, as this is already handled
+         * by "import/order".
+         */
+        ignoreDeclarationSort: true,
+      },
+    ],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
@@ -28,5 +71,17 @@ module.exports = {
         varsIgnorePattern: '^_',
       },
     ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+      },
+      node: true,
+    },
+    'import/ignore': ['joi'], // Joi has a namespace that hasn't export his methods so we need to ignore it to prevent false eslint errors. https://github.com/import-js/eslint-plugin-import/blob/v2.28.0/docs/rules/namespace.md
   },
 };
