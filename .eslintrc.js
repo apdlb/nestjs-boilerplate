634c85a6ad5f70f50b6aa7a350a29f7276fe62c1
Alejandro Páez <alepdlb@gmail.com>
2023-08-10 16:47:55 +0200

    chore: error when unused vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -65,7 +65,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': [
-      'warn',
+      'error',
       {
         argsIgnorePattern: '^_',
         varsIgnorePattern: '^_',
