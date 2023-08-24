f2813cee51ec3ee59c27a48df66c5bf8500bc53b
Alejandro Páez <alepdlb@gmail.com>
2023-08-24 14:19:07 +0200

    refactor: reorganize admin code

--- src/admin/index.ts
+++ src/admin/index.ts
@@ -1,17 +1,58 @@
-import { Prisma } from '@prisma/client';
+import { INestApplication } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import * as argon2 from 'argon2';
 
-export const datamodelMapper = () => {
-  const datamodel = Prisma.dmmf.datamodel;
+const prisma = new PrismaClient();
 
-  const models = datamodel.models.reduce(
-    (acc, model) => ((acc[model.name] = model), acc),
-    {} as Record<string, Prisma.DMMF.Model>,
-  );
+export default async function setupAdmin(
+  app: INestApplication,
+  options: {
+    port: string;
+  },
+) {
+  const AdminJS = await import('adminjs');
+  const AdminJSExpress = (await import('@adminjs/express')).default;
+  const AdminJSPrisma = await import('@adminjs/prisma');
+  const passwordsFeature = (await import('@adminjs/passwords')).default;
+  const ComponentLoader = AdminJS.ComponentLoader;
+  const componentLoader = new ComponentLoader();
 
-  const enumMap = datamodel.enums.reduce(
-    (acc, enumItem) => ((acc[enumItem.name] = enumItem), acc),
-    {} as Record<string, Prisma.DMMF.DatamodelEnum>,
-  );
+  AdminJS.default.registerAdapter({
+    Resource: AdminJSPrisma.Resource,
+    Database: AdminJSPrisma.Database,
+  });
 
-  return { models, enumMap };
-};
+  // Icons from https://feathericons.com/
+  const adminOptions = {
+    resources: [
+      {
+        resource: {
+          client: prisma,
+          model: AdminJSPrisma.getModelByName('User'),
+        },
+        features: [
+          passwordsFeature({
+            componentLoader,
+            properties: { encryptedPassword: 'password' },
+            hash: argon2.hash,
+          }),
+        ],
+        options: {
+          id: 'User',
+          navigation: {
+            name: 'Users',
+            icon: 'User',
+          },
+        },
+      },
+    ],
+    componentLoader,
+  };
+  const admin = new AdminJS.default(adminOptions);
+  const adminRouter = AdminJSExpress.buildRouter(admin);
+  app.use(admin.options.rootPath, adminRouter);
+
+  return {
+    successMessage: `AdminJS started on http://localhost:${options.port}${admin.options.rootPath}`,
+  };
+}
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,18 @@
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { PrismaClient } from '@prisma/client';
-import * as argon2 from 'argon2';
 import * as cookieParser from 'cookie-parser';
 
-import { datamodelMapper } from './admin';
+import setupAdmin from './admin';
 import { AppModule } from './app.module';
-
-const prisma = new PrismaClient();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const logger = new Logger(AppModule.name);
   const configService = app.get(ConfigService);
+
+  const port = configService.get('API_PORT') || 3000;
 
   app.enableCors({
     origin: [configService.get('WEB_URL')],
@@ -24,56 +22,14 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
-  const AdminJS = await import('adminjs');
-  const AdminJSExpress = (await import('@adminjs/express')).default;
-  const AdminJSPrisma = (await import('@adminjs/prisma')).default;
-  const passwordsFeature = (await import('@adminjs/passwords')).default;
-  const ComponentLoader = AdminJS.ComponentLoader;
-  const componentLoader = new ComponentLoader();
-
-  AdminJS.default.registerAdapter({
-    Resource: AdminJSPrisma.Resource,
-    Database: AdminJSPrisma.Database,
-  });
-
-  // Workaround - https://github.com/SoftwareBrothers/adminjs-prisma/issues/35
-  const { models, enumMap } = datamodelMapper();
-
-  const adminOptions = {
-    // We pass User to `resources`
-    resources: [
-      {
-        resource: { client: prisma, model: models['User'], enumMap },
-        features: [
-          passwordsFeature({
-            componentLoader,
-            properties: { encryptedPassword: 'password' },
-            hash: argon2.hash,
-          }),
-        ],
-        options: {
-          id: 'user',
-          navigation: {
-            name: 'Users',
-            icon: 'User',
-          },
-        },
-      },
-    ],
-    componentLoader,
-  };
-  const admin = new AdminJS.default(adminOptions);
-  const adminRouter = AdminJSExpress.buildRouter(admin);
-  app.use(admin.options.rootPath, adminRouter);
-
-  const port = configService.get('API_PORT') || 3000;
+  const admin = await setupAdmin(app, { port });
 
   await app.listen(port, () => {
-    logger.log(`Application successfully started on http://localhost:${port}`);
-
     logger.log(
-      `AdminJS started on http://localhost:${port}${admin.options.rootPath}`,
+      `Application successfully started on http://localhost:${port}/graphql`,
     );
+
+    logger.log(admin.successMessage);
   });
 }
 
