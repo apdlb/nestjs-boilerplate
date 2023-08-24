55169da12d16cd11fa09eafadf11aa9bdb8cb82f
Alejandro Páez <alepdlb@gmail.com>
2023-08-24 14:15:57 +0200

    feat: add hash to password

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,20 @@
 import { PrismaClient } from '@prisma/client';
+import * as argon2 from 'argon2';
 
 const prisma = new PrismaClient();
 
 async function main() {
+  const adminHashedPassword = await argon2.hash(
+    '59tpZv46NUn9b7fn6WXxBCilZhTfuO',
+  );
+
   await prisma.user.upsert({
     where: { email: 'admin@z1.digital' },
     update: {},
     create: {
       role: 'ADMIN',
       email: 'admin@z1.digital',
-      password: '59tpZv46NUn9b7fn6WXxBCilZhTfuO',
+      password: adminHashedPassword,
       firstName: 'Admin',
     },
   });
